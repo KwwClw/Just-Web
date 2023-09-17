fb57bec4e5951fe76081a77fe2b7d5664a4a88b5
KwwClw <neskung50@gmail.com>
2023-09-17 15:17:21 +0700

    add

--- scripts.js
+++ scripts.js
@@ -4,23 +4,14 @@ function drop() {
 
 document.addEventListener("DOMContentLoaded", function () {
     const switchmode = document.getElementById("switchmode");
-    const styleLink = document.getElementById("style-link");
-    const body = document.body;
+    const currentClass = body.className;
 
     switchmode.addEventListener("change", () => {
         if(switchmode.checked) {
-            body.classList.add("dark-mode");
-            styleLink.href = "dark-mode.css";
+            body.className = "dark-mode";
+            console.log(currentClass);
         } else {
-            body.classList.remove("dark-mode");
-            styleLink.href = "styles.css";
+            body.className = "light-mode";
         }
     });
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        switchmode.checked = true;
-        // Enable dark mode
-        body.classList.add("dark-mode");
-        // Update the stylesheet to the dark mode version
-        styleLink.href = "dark-mode.css";
-    }
 });
